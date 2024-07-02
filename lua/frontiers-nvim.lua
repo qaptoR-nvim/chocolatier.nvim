c86668450466ab6cb7eab6764902acd6324343f9
qaptoR <git@arxdaingneachd.org>
2024-07-02 12:47:08 -0700

    changed colorscheme name for telescope

--- lua/frontiers-nvim.lua
+++ lua/frontiers-nvim.lua
@@ -1209,7 +1209,7 @@ Frontiers.load = function()
   if vim.g.colors_name then
     vim.cmd.hi("clear")
   end
-  vim.g.colors_name = "frontiers"
+  vim.g.colors_name = "frontiers-nvim"
   vim.o.termguicolors = true
 
   local groups = get_groups()
